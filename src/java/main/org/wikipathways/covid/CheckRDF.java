9cb4df40c00fcd6df49c918718d105f0f80b5623
Egon Willighagen <egon.willighagen@gmail.com>
2021-10-16 16:22:38 +0200

    New upstream release: configurable tests, results in the index

--- src/java/main/org/wikipathways/covid/CheckRDF.java
+++ src/java/main/org/wikipathways/covid/CheckRDF.java
@@ -2,12 +2,14 @@ package org.wikipathways.covid;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.Writer;
+import java.io.PrintWriter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import nl.unimaas.bigcat.wikipathways.curator.assertions.*;
-import nl.unimaas.bigcat.wikipathways.curator.tests.*;
 import nl.unimaas.bigcat.wikipathways.curator.SPARQLHelper;
 
 import org.apache.jena.rdf.model.Model;
@@ -18,77 +20,43 @@ public class CheckRDF {
     public static String getHashcode(String string) { return Integer.toHexString(string.hashCode()); }
 
     public static void main(String[] args) throws Exception {
-        String wpFile   = args[0];
+        String wpFile     = args[0];
+        String reportFile = args[1];
         String gpmlFile = wpFile.replace("wp/Human", "wp/gpml/Human");
         String sbmlFile = wpFile.replace("wp/Human", "sbml").replace(".ttl",".sbml");
         String notesFile = sbmlFile.replace(".sbml",".txt");
         String svgFile  = sbmlFile.replace(".sbml",".svg");
         String wpid     = wpFile.substring(9,wpFile.indexOf(".ttl"));
-        System.out.println("<img style=\"float: right; width: 200px\" src=\"../logo.png\" />");
 
-        System.out.println("# WikiPathways " + wpid + "\n");
-        System.out.println("* WikiPathways: [" + wpid + "](https://identifiers.org/wikipathways:" + wpid + ")");
-        System.out.println("* Scholia: [" + wpid + "](https://scholia.toolforge.org/wikipathways/" + wpid + ")");
-        System.out.println("* WPRDF file: [" + wpFile + "](../" + wpFile + ")");
-        System.out.println("* GPMLRDF file: [" + gpmlFile + "](../" + gpmlFile + ")");
-        System.out.println("* SBML file: [" + sbmlFile + "](../" + sbmlFile + ") ([SVG](../" + svgFile + ")) ([conversion notes](../" + notesFile + "))\n");
+        PrintWriter report = new PrintWriter(reportFile);
+        PrintWriter reportStatus = new PrintWriter(reportFile.replace(".md",".txt"));
+
+        report.println("<img style=\"float: right; width: 200px\" src=\"../logo.png\" />");
+
+        report.println("# WikiPathways " + wpid + "\n");
+        report.println("* WikiPathways: [" + wpid + "](https://identifiers.org/wikipathways:" + wpid + ")");
+        report.println("* Scholia: [" + wpid + "](https://scholia.toolforge.org/wikipathways/" + wpid + ")");
+        report.println("* WPRDF file: [" + wpFile + "](../" + wpFile + ")");
+        report.println("* GPMLRDF file: [" + gpmlFile + "](../" + gpmlFile + ")");
+        report.println("* SBML file: [" + sbmlFile + "](../" + sbmlFile + ") ([SVG](../" + svgFile + ")) ([conversion notes](../" + notesFile + "))\n");
         List<IAssertion> assertions = new ArrayList<IAssertion>();
         Model loadedData = ModelFactory.createDefaultModel();
         loadedData.read(new FileInputStream(new File(wpFile)), "", "TURTLE");
         loadedData.read(new FileInputStream(new File(gpmlFile)), "", "TURTLE");
 
         SPARQLHelper helper = new SPARQLHelper(loadedData);
-        assertions.addAll(GeneralTests.titlesShortEnough(helper)); // not all GeneralTests
-        assertions.addAll(GeneralTests.weirdCharacterTitles(helper));
-        assertions.addAll(GeneralTests.duplicateTitles(helper));
-        assertions.addAll(GeneralTests.noTags(helper));
-        assertions.addAll(GeneralTests.nullDataSources(helper));
-        assertions.addAll(GeneralTests.undefinedDataSources(helper));
-        assertions.addAll(GeneralTests.undefinedIdentifier(helper));
-        assertions.addAll(GeneralTests.dataNodeWithoutGraphId(helper));
-        assertions.addAll(GeneralTests.groupsHaveDetail(helper));
-        assertions.addAll(GeneralTests.emptyLabelOfNodeWithIdentifier(helper));
-        assertions.addAll(DataNodesTests.all(helper));
-        assertions.addAll(PathwayTests.all(helper));
-        assertions.addAll(ReferencesTests.all(helper));
-	assertions.addAll(WikidataTests.keggWithoutMapping(helper)); // not all
-	assertions.addAll(WikidataTests.pubchemCIDWithoutMapping(helper));
-	assertions.addAll(WikidataTests.hmdbWithoutMapping(helper));
-	assertions.addAll(WikidataTests.casWithoutMapping(helper));
-	assertions.addAll(WikidataTests.wikDataTypo(helper));
-	assertions.addAll(WikidataTests.duplicateWikidataMappings(helper));
-	assertions.addAll(WikidataTests.wikidataIdentifiersWrong(helper));
-	assertions.addAll(WikidataTests.chemspiderCIDWithoutMapping(helper));
-        assertions.addAll(OutdatedDataSourcesTests.outdatedUniprot(helper));
-        assertions.addAll(OutdatedDataSourcesTests.outdatedUniprot2(helper));
-        assertions.addAll(OutdatedDataSourcesTests.outdatedUniprot3(helper));
-        assertions.addAll(OutdatedDataSourcesTests.outdatedUniprot4(helper));
-        assertions.addAll(OutdatedDataSourcesTests.oldUniprotSwissProt(helper));
 
-        assertions.addAll(CovidDiseaseMapsTests.all(helper));
+        Scanner testConfigFile = new Scanner(new FileInputStream("tests.txt"));
+        while (testConfigFile.hasNextLine()) {
+            String testConfig = testConfigFile.nextLine().trim();
+            if (!testConfig.startsWith("#")) { // comment lines start with #
+                String[] config = testConfig.split("\\.");
+                Class testClass = Class.forName("nl.unimaas.bigcat.wikipathways.curator.tests." + config[0]);
+                assertions.addAll((List<IAssertion>)testClass.getDeclaredMethod(config[1], SPARQLHelper.class).invoke(null, helper));
+            }
+        }
 
-        assertions.addAll(GeneTests.all(helper));
-        assertions.addAll(EnsemblTests.wrongEnsemblIDForHumanSpecies(helper)); // exclude outdatedIdentifiers (not all pathways have many genes)
-        assertions.addAll(EnsemblTests.wrongEnsemblIDForRatSpecies(helper));
-        assertions.addAll(EnsemblTests.wrongEnsemblIDForCowSpecies(helper));
-        assertions.addAll(EnsemblTests.wrongEnsemblIDForMouseSpecies(helper));
-        assertions.addAll(ProteinsTests.all(helper));
-        assertions.addAll(UniProtTests.all(helper));
-
-        assertions.addAll(CASMetabolitesTests.all(helper));
-        assertions.addAll(ChEBIMetabolitesTests.all(helper));
-        assertions.addAll(ChemSpiderTests.all(helper));
-        assertions.addAll(HMDBMetabolitesTests.outdatedIdentifiers(helper)); // not all
-        assertions.addAll(HMDBSecMetabolitesTests.all(helper));
-        assertions.addAll(KEGGMetaboliteTests.all(helper));
-        assertions.addAll(LIPIDMAPSTests.all(helper));
-        assertions.addAll(MetabolitesTests.all(helper));
-        assertions.addAll(MetaboliteStructureTests.all(helper));
-        assertions.addAll(PubChemMetabolitesTests.all(helper));
-
-        assertions.addAll(InteractionTests.all(helper));
-
-        System.out.println("## Tests");
+        report.println("## Tests");
 
         List<IAssertion> failedAssertions = new ArrayList<IAssertion>();
         int testClasses = 0;
@@ -97,6 +65,7 @@ public class CheckRDF {
         String currentTestClass = "";
         String currentTest = "";
         boolean currentTestClassHasFails = false;
+        boolean anyTestClassHasFails = false;
         String currentTestClassMessages = "";
         String message = "";
         String errors = "";
@@ -111,9 +80,10 @@ public class CheckRDF {
 
                   // only output results when there are fails
                   if (currentTestClassHasFails) {
-                    System.out.println(currentTestClassMessages + message);
+                    report.println(currentTestClassMessages + message);
+                    anyTestClassHasFails = true;
                   } else {
-                    System.out.println(": all " + testClassTests + " tests OK!");
+                    report.println(": all " + testClassTests + " tests OK!");
                   }
                 }
 
@@ -124,7 +94,7 @@ public class CheckRDF {
                 currentTestClassMessages = "";
                 message = "";
                 testClassTests = 0;
-                System.out.print("* " + currentTestClass);
+                report.print("* " + currentTestClass);
                 currentTestClassHasFails = false;
                 currentTestClassMessages = "";
             }
@@ -202,30 +172,39 @@ public class CheckRDF {
           if (!errors.isEmpty()) message += "\n" + errors;
         }
         if (currentTestClassHasFails) {
-          System.out.println(currentTestClassMessages + message);
+          report.println(currentTestClassMessages + message);
+          anyTestClassHasFails = true;
         } else {
-          System.out.println(": all " + testClassTests + " tests OK!");
+          report.println(": all " + testClassTests + " tests OK!");
         }
 
-        System.out.println();
-        System.out.println("\n## Summary\n");
-        System.out.println("* Number of test classes: " + testClasses);
-        System.out.println("* Number of tests: " + tests);
-        System.out.println("* Number of assertions: " + assertions.size());
-        System.out.println("* Number of fails: " + failedAssertions.size());
+        report.println();
+        report.println("\n## Summary\n");
+        report.println("* Number of test classes: " + testClasses);
+        report.println("* Number of tests: " + tests);
+        report.println("* Number of assertions: " + assertions.size());
+        report.println("* Number of fails: " + failedAssertions.size());
 
-        System.out.println("\n## Fails\n");
+        report.println("\n## Fails\n");
         for (IAssertion assertion : failedAssertions) {
-            System.out.println("<a name=\"" + getHashcode(assertion.getTestClass() + assertion.getTest() + assertion.getMessage()) + "\" />\n");
-            System.out.println("## " + assertion.getTestClass() + "." + assertion.getTest());
-            System.out.println("\n" + assertion.getMessage());
+            report.println("<a name=\"" + getHashcode(assertion.getTestClass() + assertion.getTest() + assertion.getMessage()) + "\" />\n");
+            report.println("## " + assertion.getTestClass() + "." + assertion.getTest());
+            report.println("\n" + assertion.getMessage());
             if (assertion.getDetails() != null && !assertion.getDetails().isEmpty()) {
-                System.out.println("```\n" + assertion.getDetails() + "```\n");
+                report.println("```\n" + assertion.getDetails() + "```\n");
                 if (assertion.hasLinkToDocs()) {
-                    System.out.println("More details at [" + assertion.getLinkToDocs() + "](" + assertion.getLinkToDocs() + ")\n");
+                    report.println("More details at [" + assertion.getLinkToDocs() + "](" + assertion.getLinkToDocs() + ")\n");
                 }
             }
         }
+        if (anyTestClassHasFails) {
+          reportStatus.println("status=⨯");
+        } else {
+          reportStatus.println("status=✓");
+        }
+
+        report.flush(); report.close();
+        reportStatus.flush(); reportStatus.close();
     }
 
 }
